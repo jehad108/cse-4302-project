f7d89570d6dbc7d059481490501b57c46539c58c
jihad-220041108 <abubakar22@iut-dhaka.edu>
2024-10-05 04:54:17 +0600

    lets go

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,27 +1,84 @@
 #include <raylib.h>
-#include "ball.h"
+
+class SpriteAnimation
+{
+private:
+    Texture2D spriteSheet;
+    Rectangle sourceRect;
+    Vector2 position;
+    int frameCount;
+    int currentFrame;
+    float frameTime;
+    float elapsedTime;
+
+public:
+    // Constructor
+    SpriteAnimation(const char* imagePath, int totalFrames, float fps, Vector2 pos)
+    {
+        spriteSheet = LoadTexture(imagePath);
+        frameCount = totalFrames;
+        frameTime = 1.0f / fps;
+        elapsedTime = 0.0f;
+        currentFrame = 0;
+        position = pos;
+        sourceRect = {0, 0, (float)spriteSheet.width / frameCount, (float)spriteSheet.height};
+    }
+
+    // Destructor to unload the texture
+    ~SpriteAnimation()
+    {
+        UnloadTexture(spriteSheet);
+    }
+
+    // Update method to handle frame switching
+    void Update()
+    {
+        elapsedTime += GetFrameTime();
+        if (elapsedTime >= frameTime)
+        {
+            currentFrame = (currentFrame + 1) % frameCount;
+            sourceRect.x = currentFrame * (float)spriteSheet.width / frameCount;
+            elapsedTime = 0.0f;
+        }
+    }
+
+    // Draw method to render the current frame
+    void Draw()
+    {
+        DrawTextureRec(spriteSheet, sourceRect, position, WHITE);
+    }
+
+    // Method to change the FPS dynamically
+    void SetFPS(float fps)
+    {
+        frameTime = 1.0f / fps;
+    }
+};
 
 int main()
 {
-    Color darkGreen = Color{20, 160, 133, 255};
+    const int windowWidth = GetScreenWidth();
+    const int windowHeight = GetScreenHeight();
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+    InitWindow(windowWidth, windowHeight, "OOP Sprite Animation");
+    ToggleFullscreen();
 
-    Ball ball = Ball();
+    SpriteAnimation menuBackground("images/menu.png", 8, 45.0f, {0, 0});
 
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        menuBackground.Update();
+
         BeginDrawing();
-        ClearBackground(darkGreen);
-        ball.Update();
-        ball.Draw();
+        ClearBackground(WHITE);
+        menuBackground.Draw();
         EndDrawing();
     }
 
     CloseWindow();
+
     return 0;
 }
+
